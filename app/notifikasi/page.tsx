dd29dd391ce2f1628bc5c9a1b48090f8071df937
Thesambal <zilanzalilan18@gmail.com>
2026-04-24 23:48:25 +0700

    membetulkan toggle nafbar di halaman notifikasi untuk mode mobile

--- app/notifikasi/page.tsx
+++ app/notifikasi/page.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
 import NotificationList from "@/components/NotificationList";
+import { useState } from "react";
 
 export default function NotifikasiPage() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="flex min-h-screen bg-surface">
-      <Sidebar />
+      <Sidebar isOpen={isSidebarOpen} onToggle={setIsSidebarOpen} />
       
-      <main className="pt-16 lg:pt-0 lg:ml-64 flex-1 min-h-screen">
-        <Header title="Notifikasi" />
+      <main className="lg:ml-64 flex-1 min-h-screen">
+        <Header 
+          title="Notifikasi"
+          onToggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}
+          isSidebarOpen={isSidebarOpen}
+        />
         
         <div className="p-4 lg:p-8 max-w-4xl mx-auto">
           <NotificationList />
